7c97d39ec0b37c8d22753b16e51654f3d673d215
AlexStauch <alexnstauch@gmail.com>
2020-03-12 11:14:55 -0700

    second commit

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 
 let drops = [];
 let howManyDrops = 10000;
-
+let whichDrop = 0;
 
 function setup(){
-	createCanvas(windowWidth, windowHeight, WEBGL);
+	createCanvas(1000, 1000, WEBGL);
 	for(i = 0; i < howManyDrops; i ++){
 		drops[i] = new Drops();
 	}
@@ -16,6 +16,7 @@ function draw(){
 	orbitControl(3, 3, 3);
 	for(i = 0; i < howManyDrops; i ++){
 		drops[i].makeDropsVisible();
+		drops[i].move();
 		drops[i].display();
 	}
 }
@@ -26,6 +27,9 @@ class Drops {
 		this.x = random(-width, width);
 		this.y = random(-height, height);
 		this.z = random(-5000, 5000);
+		this.xspeed = (0,1);
+		this.yspeed = (0,1);
+		this.zspeed = (0,5);
 		this.visible = false;
 	}
 	display(){
@@ -35,6 +39,23 @@ class Drops {
 			fill(209, 156, 42);
 			sphere(10, 2, 2);
 			pop();
+		}
+	}
+	move(){
+		this.x = this.x + this.xspeed;
+		this.y = this.y + this.yspeed;
+		this.z = this.z + this.zspeed;
+
+		if((this.x > width) || this.x < 0){
+			this.xspeed = this.xspeed * -1;
+		}
+
+		if((this.y > height) || this.y < 0){
+			this.yspeed = this.yspeed * -1;
+		}
+
+		if((this.z > 5000) || this.z < 0){
+			this.zspeed = this.zspeed * -1;
 		}
 	}
 	makeDropsVisible(){
